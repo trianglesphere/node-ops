2252d75042fdf4ef9f89fc6d2ea43a76d43ae33e
Joshua Gutow <jgutow@oplabs.co>
2023-03-15 14:45:41 -0700

    Add optimism

---
+++ create_dirs.sh
@@ -0,0 +1,5 @@
+mkdir -p ethereum/datadirs/goerli/geth
+mkdir -p ethereum/datadirs/goerli/lighthouse
+mkdir -p ethereum/datadirs/mainnet/geth
+mkdir -p ethereum/datadirs/mainnet/lighthouse
+mkdir -p optimism/datadirs/goerli/geth
--- regen_config.sh
+++ regen_config.sh
@@ -1,3 +1,3 @@
 mkdir -p .config
-openssl rand -hex 32 > .config/l1-jwt-secret.txt
-openssl rand -hex 32 > .config/l2-jwt-secret.txt
+openssl rand -hex 32 > ethereum/.config/l1-jwt-secret.txt
+openssl rand -hex 32 > optimism/.config/l2-jwt-secret.txt
